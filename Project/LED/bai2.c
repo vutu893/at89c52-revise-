6c8f1fb65a5a0ac8c55262d14a16eb21d2c6578b
vutrinhtu <vutubtt893@gmail.com>
2024-07-31 23:46:28 +0700

    [CREATE] dung bit operator de tao cac hieu ung led khac nhau

---
+++ Project/LED/bai2.c
@@ -0,0 +1,57 @@
+#include <REGX52.H>
+#include "../../Lib/delay.h"
+
+//bai2: su dung bit operator de tao hieu ung led
+
+//chan Led
+#define LED_PIN P2
+
+void mode1();
+void mode2();
+void mode3();
+void mode4();
+void mode5();
+
+
+void main()
+{
+	while(1)
+	{
+		mode1();
+		delay_ms(500);
+		mode2();
+		delay_ms(500);
+		mode3();
+		delay_ms(500);
+		mode4();
+		delay_ms(500);
+		mode5();
+		delay_ms(500);
+	}
+}
+//mode1
+void mode1()
+{
+
+}
+
+//mode2
+void mode2()
+{
+
+}
+//mode3
+void mode3()
+{
+
+}
+//mode4
+void mode4()
+{
+
+}
+//mode5
+void mode5()
+{
+
+}
