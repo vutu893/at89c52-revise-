a249c7e40dbde09fc45d649de845f00a778f507c
vutrinhtu <vutubtt893@gmail.com>
2024-08-05 23:44:35 +0700

    [CREATE] bat tat che do den led voi hai button

---
+++ Project/BUTTON/bai2.c
@@ -0,0 +1,15 @@
+#include <REGX52.H>
+#include "../../Lib/delay.h"
+
+//dung hai button de dieu khien cac che do den led: mot den bat mot den tat
+#define LED_PIN P2
+sbit BTN_ON = P1^0;
+sbit BTN_OFF = P1^1;
+
+void main()
+{
+	while(1)
+	{
+		
+	}
+}
