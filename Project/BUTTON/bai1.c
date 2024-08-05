3cb27b83eb8598b2bdf4f765bc03c284af9cfb07
vutrinhtu <vutubtt893@gmail.com>
2024-08-05 23:30:09 +0700

    [ADD] delay 20ms de chong doi phim, va thao tac xu ly giu phim bang while

--- Project/BUTTON/bai1.c
+++ Project/BUTTON/bai1.c
@@ -8,8 +8,21 @@ sbit LED_PIN = P2^0;
 
 void main()
 {
+	LED_PIN = 0;
+	
 	while(1)
 	{
-	
+		if(BUTTON_PIN == 0)
+		{
+			//delay 20 ms de cho qua khoang thoi gian tin hieu bi nhieu(doi phim)
+			delay_ms(20);
+			if(BUTTON_PIN == 0)
+			{
+				LED_PIN = !LED_PIN;
+				// xu ly truong hop phim bi giu
+				while(BUTTON_PIN == 0);
+				LED_PIN = !LED_PIN;
+			}
+		}
 	}
 }
