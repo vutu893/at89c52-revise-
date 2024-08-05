87f4a230f8642fa8055eb75a08a2432d56256d58
vutrinhtu <vutubtt893@gmail.com>
2024-08-05 23:24:18 +0700

    [CREATE] thao tac voi nut nhan: chong doi phim, xu ly truong hop giu phim

---
+++ Project/BUTTON/bai1.c
@@ -0,0 +1,15 @@
+#include <REGX52.H>
+#include "../../Lib/delay.h"
+
+
+//thao tac voi nut nhan: chong doi phim, giu phim, ...
+sbit BUTTON_PIN = P1^0;
+sbit LED_PIN = P2^0;
+
+void main()
+{
+	while(1)
+	{
+	
+	}
+}
