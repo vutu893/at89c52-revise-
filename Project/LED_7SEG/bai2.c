243cc75d0666d3be278f64eedaf3e8768f87f692
vutrinhtu <vutubtt893@gmail.com>
2024-08-06 23:17:18 +0700

    [ADD] dem tu 0 den 99 bang 2 led don

--- Project/LED_7SEG/bai2.c
+++ Project/LED_7SEG/bai2.c
@@ -1,11 +1,24 @@
 #include <REGX52.H>
 #include "../../Lib/delay.h"
 
-//bai2: dem tu 0 den 99 bang led 7 seg
+//bai2: dem tu 0 den 99 bang led 7 seg dung 2 led don
+
+#define 7SEG_PIN1 P2
+#define 7SEG_PIN2 P3
+unsigned char led_7seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0x8F, 0x80, 0x90};
+
 void main()
 {
+	unsigned char i;
+	unsigned char chuc, donvi;
 	while(1)
 	{
-	
+		for(i = 0; i < 100; i++)
+		{
+			chuc = i/10;
+			7SEG_PIN1 = led_7seg[chuc];
+			donvi = i%10;
+			7SEG_PIN2 = led_7seg[donvi];
+		}
 	}
 }
