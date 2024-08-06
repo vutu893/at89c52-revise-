b962fe746b24f35fb020323c8ff69179ac29410f
vutrinhtu <vutubtt893@gmail.com>
2024-08-06 23:10:29 +0700

    [ADD] add ma code cua led 7seg tu 0 den 9

--- Project/LED_7SEG/bai1.c
+++ Project/LED_7SEG/bai1.c
@@ -2,11 +2,17 @@
 #include "../../Lib/delay.h"
 
 //dem tu 1 den 9 voi led 7seg chung catot
+#define 7SEG_PIN P2
+
+//ma hex cua cac so tu 0 den 9
+unsigned char 7seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0x8F, 0x80, 0x90};
 
 void main()
 {
+	unsigned char i, counter = 0;
+	
 	while(1)
 	{
-	
+		
 	}
 }
